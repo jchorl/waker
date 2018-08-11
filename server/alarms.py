f238ad4f371d437947ad8bafd46bac061afd2c0b
Josh Chorlton <jchorlton@gmail.com>
2018-08-11 12:05:09 -0400

    scheduling works

--- server/alarms.py
+++ server/alarms.py
@@ -1,2 +1,59 @@
+from itertools import groupby
+import schedule
+from uuid import uuid4
+
+
+"""
+an alarm is comprised of multiple jobs, one for each day it should run
+the alarm is uniquely identified by a uuid, which all the jobs are tagged by
+alarms are specified by a dict:
+    {
+        id: some_uuid,
+        time: {
+            hour: 10,
+            minute: 0
+        },
+        repeat: true,
+        days: ['monday', 'tuesday'] // days only need to be provided if repeat is true
+                                    // otherwise the alarm will just run at that time once
+    }
+"""
+
+def alarm_job():
+    print("I'm working...")
+
+def alarm_job_once():
+    print("I'm working once...")
+    return schedule.CancelJob
+
 def get_alarms():
-    return []
+    # group jobs by tag
+    jobs = schedule.jobs
+    sorted_jobs = sorted(jobs, key=lambda job: next(iter(job.tags)))
+
+    alarms = []
+    for alarm_uuid, alarm_jobs in groupby(sorted_jobs, lambda job: next(iter(job.tags))):
+        alarm = {
+                'id': alarm_uuid,
+                'days': []
+                }
+        for alarm_job in alarm_jobs:
+            alarm['days'].append(alarm_job.start_day)
+            alarm['time'] = {
+                    'hour': alarm_job.at_time.hour,
+                    'minute': alarm_job.at_time.minute,
+                    } # overwrite the time each time, they should all be the same anyway
+        alarms.append(alarm)
+    return alarms
+
+def new_alarm(alarm_info):
+    alarm_id = uuid4()
+    alarm_time = '{hour:02d}:{minute:02d}'.format(**alarm_info['time'])
+    if alarm['repeat']:
+        for day in alarm_info['days']:
+            job = schedule.every().week
+            job.start_day = day
+            job.at(alarm_time).tag(alarm_id).do(alarm_job)
+    else:
+        schedule.every().day.at(alarm_time).do(alarm_job_once)
+    return alarm_info
--- server/main.py
+++ server/main.py
@@ -1,11 +1,30 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import schedule
+import threading
+import time
 
-from alarms import get_alarms
+from alarms import get_alarms, new_alarm
 
 
+# Flask routing
 app = Flask(__name__)
 
 @app.route('/alarms', methods=['GET'])
 def get_alarms_handler():
     alarms = get_alarms()
     return jsonify(alarms)
+
+@app.route('/alarms', methods=['POST'])
+def post_alarm_handler():
+    alarm = new_alarm(request.get_json())
+    return jsonify(alarm)
+
+# alarm execution thread
+class ScheduleThread(threading.Thread):
+    @classmethod
+    def run(cls):
+        schedule.run_pending()
+        time.sleep(1)
+
+continuous_thread = ScheduleThread()
+continuous_thread.start()
