edba416339e6cfe2b4910be503ad4833961f4f53
Josh Chorlton <jchorlton@gmail.com>
2019-02-11 08:18:26 -0800

    Moving from yahoo weather to accuweather
    yahoo weather is deprecated

--- server/weather_forecast.py
+++ server/weather_forecast.py
@@ -1,7 +1,22 @@
-from weather import Weather, Unit
-from config import YAHOO_WOEID
+import requests
+
+from config import ACCUWEATHER_LOCATION_KEY
+from secrets import ACCUWEATHER_API_KEY
 
 def get_forecast():
-    weather = Weather(unit=Unit.CELSIUS)
-    lookup = weather.lookup(YAHOO_WOEID)
-    return 'Today\'s forecast is {} with a high of {} degrees and a low of {} degrees. '.format(lookup.forecast[0].text, lookup.forecast[0].high, lookup.forecast[0].low)
+    resp = send_req()
+    minimum = int(round(resp['DailyForecasts'][0]['Temperature']['Minimum']['Value']))
+    maximum = int(round(resp['DailyForecasts'][0]['Temperature']['Maximum']['Value']))
+    phrase = resp['DailyForecasts'][0]['Day']['IconPhrase']
+    return 'Today\'s forecast is {} with a high of {} degrees and a low of {} degrees. '.format(phrase, maximum, minimum)
+
+def send_req():
+    payload = {
+        'apikey': ACCUWEATHER_API_KEY,
+        'language': 'en-us',
+        'details': 'false',
+        'metric': 'true',
+    }
+    url = 'https://dataservice.accuweather.com/forecasts/v1/daily/1day/{}'.format(ACCUWEATHER_LOCATION_KEY)
+    resp = requests.get(url, params=payload)
+    return resp.json()
